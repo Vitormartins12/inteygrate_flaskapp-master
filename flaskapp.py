d6efd2c5914db11b9883ddc7ae0d0afc7eefedb5
Vitormartins12 <vitorsmap@gmail.com>
2017-10-24 22:07:24 -0200

    Initial commit

---
+++ flaskapp.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask import request
+
+#WA[
+from whatsapp import Client
+expected_token = 'AstoneEtop'
+#]WA
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def hello_world():
+
+    return 'Hello World!'
+
+@app.route('/msg', methods = ['POST'])
+def msg():
+    to = request.form['to']
+    return str(to)
+
+@app.route('/sendmsg')
+def sendmsg():
+    to = request.args.get('to')
+    msg = request.args.get('msg')
+    token = request.args.get('token')
+    if(str(token) == expected_token):
+        client = Client(login='5521980563420', password='VGh4swEc7sT60SyhUd+vBbY8UgA=')
+        res = client.send_message(to, msg)  
+    
+    else:
+        res = 'Unauthorized'
+    
+    return str(res)
+
+if __name__ == '__main__':
+    app.run()
