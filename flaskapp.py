20aab967baa41fe3780a40c10e08446e71dbbcbe
vitorsmap <vitorsmap@gmail.com>
2017-11-17 14:40:58 -0200

    Add files via upload

--- flaskapp.py
+++ flaskapp.py
@@ -3,7 +3,7 @@ from flask import request
 
 #WA[
 from whatsapp import Client
-expected_token = 'AstoneEtop'
+expected_token = 'gKcX7OpJ9C7qlVwyoRDDvh1xE80='
 #]WA
 
 app = Flask(__name__)
@@ -26,7 +26,7 @@ def sendmsg():
     msg = request.args.get('msg')
     token = request.args.get('token')
     if(str(token) == expected_token):
-        client = Client(login='5521980563420', password='VGh4swEc7sT60SyhUd+vBbY8UgA=')
+        client = Client(login='55980581902', password='DaDu/8fhurura31InnauujB4vog='')
         res = client.send_message(to, msg)  
     
     else:
